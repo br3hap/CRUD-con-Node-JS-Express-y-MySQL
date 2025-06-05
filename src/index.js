3112a75083c3b10b31b7fc6177c370e81e0121ba
Breithner <Breithnerap@gmail.com>
2025-06-04 21:19:44 -0500

    upload change

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import { engine } from 'express-handlebars';
+import morgan from 'morgan';
+import {join, dirname} from 'path'
+import { fileURLToPath } from 'url';
+
+
+//inicialization
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+//settings
+app.set('port', process.env.PORT || 3000)
+app.set('views', join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    defaultLayout:'main',
+    layoutsDir: join(app.get('views'), 'layouts'),
+    layoutsDir: join(app.get('views'), 'partials'),
+    extname:'.hbs'
+
+}));
+app.set('view engine', '.hbs')
+
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//Routes
+app.get('/', (req, res)=> {
+    res.json({"message":"Hola"})
+});
+
+//Public Files
+app.use(express.static(join(__dirname, 'public')));
+
+
+//Run Server
+app.listen(app.get('port'), ()=>
+    console.log('Server listening on port', app.get('port')));
